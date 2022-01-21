04dff2ef8a938dd2199e1998b9e7de7d703aa507
gaponukz <49754258+gaponukz@users.noreply.github.com>
2022-01-22 01:59:20 +0200

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -8,12 +8,12 @@ contract Token is ERC20 {
     function transfer(address from, address to, uint256 amount) virtual override public {
         uint256 commission = amount / 100;
         require(balances[from] > (amount + commission));
-        require(allowed[msg.sender][from] > (amount + commission));
+        require(allowed[from][msg.sender] > (amount + commission));
 
         super.transfer(from, to, amount);
 
         balances[from] -= commission;
-        allowed[msg.sender][from] -= commission;
+        allowed[from][msg.sender] -= commission;
         balances[msg.sender] += commission;
     }
 }
